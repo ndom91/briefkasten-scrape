0e89cb7429e8abc9ac127f5bb521f3026f40b742
ndom91 <yo@ndo.dev>
2022-06-03 22:51:06 +0200

    feat: run GH Action every 2 hrs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userId = 'cl2z766os0010tfbh1lh04u3s'
 ;(async () => {
   try {
     const connection = await mysql.createConnection(process.env.DATABASE_URL ?? '');
-    const [readRows] = await connection.execute('SELECT id, url FROM `Bookmark` WHERE `userId` = ? AND `image` IS NULL LIMIT 3', [userId])
+    const [readRows] = await connection.execute('SELECT id, url FROM `Bookmark` WHERE `userId` = ? AND `image` IS NULL LIMIT 5', [userId])
 
     if (readRows.length === 0) {
       console.log('No bookmarks to screenshot found!')
