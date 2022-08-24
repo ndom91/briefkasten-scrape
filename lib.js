ee1936c0fda78b78354504873a6ec3378460bb39
ndom91 <yo@ndo.dev>
2022-08-24 10:08:10 +0200

    feat: add another error to unsplash list

--- lib.js
+++ lib.js
@@ -54,6 +54,7 @@ const fetchImage = async (url, id) => {
       e.message.includes('ERR_CONNECTION_REFUSED') ||
       e.message.includes('ERR_NAME_NOT_RESOLVED') ||
       e.message.includes('ERR_ABORTED') ||
+      e.message.includes('Execution context was destroyed') ||
       e.message.includes('ERR_TOO_MANY_REDIRECTS')
     ) {
       const client = new Client({
